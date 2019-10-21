b7fbfd0defae2abc962dc449be7b92adf030e8bf
Edgardo <Edgardo@DESKTOP-HRVT699>
2019-10-21 18:17:33 -0300

    recomenzamos con el proyecto.

---
+++ src/ar/com/tbf/datatables/response/DataTablesResponse.java
@@ -0,0 +1,49 @@
+package ar.com.tbf.datatables.response;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+public class DataTablesResponse {
+
+	private int draw;
+	private int filteredResultsCount;
+	private int totalResultsCount;
+	private List<?> data;
+	
+	public void build( Page<?> page ) {
+		
+		filteredResultsCount = page.getTotalPages() * page.getSize();
+		totalResultsCount    = (int) page.getTotalElements();
+		
+		setData(page.getContent());
+	}
+	
+	public int getDraw() {
+		return draw;
+	}
+	public void setDraw(int draw) {
+		this.draw = draw;
+	}
+	public int getFilteredResultsCount() {
+		return filteredResultsCount;
+	}
+	public void setFilteredResultsCount(int filteredResultsCount) {
+		this.filteredResultsCount = filteredResultsCount;
+	}
+	public int getTotalResultsCount() {
+		return totalResultsCount;
+	}
+	public void setTotalResultsCount(int totalResultsCount) {
+		this.totalResultsCount = totalResultsCount;
+	}
+
+	public List<?> getData() {
+		return data;
+	}
+
+	public void setData(List<?> data) {
+		this.data = data;
+	}
+	
+}
