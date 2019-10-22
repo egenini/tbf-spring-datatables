58b1d6cf5fe46a188edc720f6254d86d45c04606
Edgardo <Edgardo@DESKTOP-HRVT699>
2019-10-21 21:37:47 -0300

    Se corrige la respuesta para que datatables tome correctamente los datos de las paginación.

--- src/ar/com/tbf/datatables/response/DataTablesResponse.java
+++ src/ar/com/tbf/datatables/response/DataTablesResponse.java
@@ -6,44 +6,39 @@ import org.springframework.data.domain.Page;
 
 public class DataTablesResponse {
 
-	private int draw;
-	private int filteredResultsCount;
-	private int totalResultsCount;
+	private Integer draw = 1;
+	private Integer recordsTotal;
+	private Integer recordsFiltered = 0;
 	private List<?> data;
-	
-	public void build( Page<?> page ) {
-		
-		totalResultsCount    = (int) page.getTotalElements();
-		filteredResultsCount = totalResultsCount;
-		
-		setData(page.getContent());
-	}
-	
-	public int getDraw() {
-		return draw;
-	}
-	public void setDraw(int draw) {
-		this.draw = draw;
-	}
-	public int getFilteredResultsCount() {
-		return filteredResultsCount;
-	}
-	public void setFilteredResultsCount(int filteredResultsCount) {
-		this.filteredResultsCount = filteredResultsCount;
-	}
-	public int getTotalResultsCount() {
-		return totalResultsCount;
-	}
-	public void setTotalResultsCount(int totalResultsCount) {
-		this.totalResultsCount = totalResultsCount;
+
+	public void build(Page<?> page) {
+
+		this.setRecordsTotal( (int) page.getTotalElements() );
+		this.setData(         page.getContent()             );
 	}
 
+	public Integer getDraw() {
+		return draw;
+	}
+	public void setDraw(Integer draw) {
+		this.draw = draw;
+	}
+	public Integer getRecordsTotal() {
+		return recordsTotal;
+	}
+	public void setRecordsTotal(Integer recordsTotal) {
+		this.recordsTotal = recordsTotal;
+	}
+	public Integer getRecordsFiltered() {
+		return recordsFiltered == 0 ? this.getRecordsTotal() : this.recordsFiltered;
+	}
+	public void setRecordsFiltered(Integer recordsFiltered) {
+		this.recordsFiltered = recordsFiltered;
+	}
 	public List<?> getData() {
 		return data;
 	}
-
 	public void setData(List<?> data) {
 		this.data = data;
 	}
-	
 }
